93c9fac8757be23b2e3f70c0f4e8fbe6d30cda18
Tehoor Marjan <tehoor.marjan@gmail.com>
2026-01-12 00:47:15 +0100

    Clean the crayon interface

--- twitch/src/twitch_crayon/cli.py
+++ twitch/src/twitch_crayon/cli.py
@@ -1,113 +1,35 @@
 import asyncio
 import os
-import re
-import unicodedata
-
 import click
-import httpx
+
 from dotenv import load_dotenv
-from twitchAPI.chat import Chat, ChatCommand, ChatMessage, ChatSub, EventData
+from twitchAPI.chat import Chat, ChatCommand, EventData
 from twitchAPI.oauth import UserAuthenticator
 from twitchAPI.twitch import Twitch
 from twitchAPI.type import AuthScope, ChatEvent
 
-from .colors import COLORS, EFFECTS
-
-type Color = tuple[int, int, int]
+from .crayon import CrayonTwitchController
 
 load_dotenv()
 
 APP_ID = os.environ["APP_ID"]
 APP_SECRET = os.environ["APP_SECRET"]
-USER_SCOPE = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
 TARGET_CHANNEL = os.environ["TARGET_CHANNEL"]
+USAGE_URL = os.environ["USAGE_URL"]
 TOKEN = os.environ["HATOKEN"]
 BASE_URL = os.environ["HAURL"]
 
-HEADERS = {
-    "Authorization": f"Bearer {TOKEN}",
-    "content-type": "application/json",
-}
+USER_SCOPE = [AuthScope.CHAT_READ, AuthScope.CHAT_EDIT]
 
-_re_rgbparam = re.compile(r"^\s*(?P<r>\d+)\s*,\s*(?P<g>\d+)\s*,\s*(?P<b>\d+)")
-_re_hexparam = re.compile(
-    r"^\s*#(?P<r>[0-9a-fA-F]{2})(?P<g>[0-9a-fA-F]{2})(?P<b>[0-9a-fA-F]{2})"
-)
-_re_hexparam2 = re.compile(
-    r"^\s*#(?P<r>[0-9a-fA-F])(?P<g>[0-9a-fA-F])(?P<b>[0-9a-fA-F])"
-)
-_re_nameparam = re.compile(r"^\s*(?P<name>\w+)$")
-
-
-def parse_color_param(param: str) -> tuple[Color, str] | None:
-    if m := _re_rgbparam.match(param):
-        r = int(m["r"])
-        g = int(m["g"])
-        b = int(m["b"])
-        if r > 255 or g > 255 or b > 255:
-            return None
-    elif m := _re_hexparam.match(param):
-        r = int(m["r"], 16)
-        g = int(m["g"], 16)
-        b = int(m["b"], 16)
-    elif m := _re_hexparam2.match(param):
-        r = int(m["r"] * 2, 16)
-        g = int(m["g"] * 2, 16)
-        b = int(m["b"] * 2, 16)
-    elif m := _re_nameparam.match(param):
-        name = (
-            unicodedata.normalize("NFD", m["name"])
-            .encode("ascii", "ignore")
-            .decode("utf-8")
-            .capitalize()
-        )
-        if name in COLORS:
-            return COLORS[name], "None"
-        elif name in EFFECTS:
-            return (0, 0, 0), name
-        return None
-    else:
-        return None
-    return (r, g, b), "None"
-
-
-async def crayon_send_command(color: Color, effect: str):
-    r, g, b = color
-    data = {
-        "entity_id": "light.crayon_rgb_light",
-        "effect": effect,
-        "brightness": 255,
-    }
-    if effect == "None":
-        data["rgb_color"] = [r, g, b]
-    async with httpx.AsyncClient() as client:
-        response = await client.post(
-            BASE_URL + "api/services/light/turn_on", headers=HEADERS, json=data
-        )
-        return response.status_code == httpx.codes.OK
+crayon = CrayonTwitchController(base_url=BASE_URL, token=TOKEN)
 
 # ## Twitch Chat Commands
-
-# this will be called whenever the !reply command is issued
-async def test_command(cmd: ChatCommand):
-    if len(cmd.parameter) == 0:
-        await cmd.reply("you did not tell me what to reply with")
-    else:
-        await cmd.reply(f"{cmd.user.name}: {cmd.parameter}")
-
 async def crayon_command(cmd: ChatCommand):
-    if len(cmd.parameter) == 0:
-        await cmd.reply(f"Please provide a color parameter.")
-        return
-    color_effect = parse_color_param(cmd.parameter)
-    if color_effect is None:
-        await cmd.reply(f"Invalid color parameter.")
-        return
-    success = await crayon_send_command(*color_effect)
-    if success:
-        await cmd.reply(f"Crayon color changed!")
-    else:
-        await cmd.reply(f"Failed to change crayon color.")
+    reply = await crayon.do_twitch_command(cmd.parameter)
+    await cmd.reply(reply)
+
+async def usage_command(cmd: ChatCommand):
+    await cmd.reply(f"Utilisation du bot : {USAGE_URL}")
 
 # This will be called when the event READY is triggered, which will be on
 # bot start
@@ -124,7 +46,7 @@ async def run():
     # set up twitch api instance and add user authentication with some scopes
     twitch = await Twitch(APP_ID, APP_SECRET)
     auth = UserAuthenticator(twitch, USER_SCOPE)
-    token, refresh_token = await auth.authenticate()
+    token, refresh_token = await auth.authenticate()  # type: ignore
     await twitch.set_user_authentication(token, USER_SCOPE, refresh_token)
 
     # create chat instance
@@ -135,10 +57,9 @@ async def run():
     # listen to when the bot is done starting up and ready to join channels
     chat.register_event(ChatEvent.READY, on_ready)
 
-    # you can directly register commands and their handlers,
-    # this will register the !reply command
-    chat.register_command("reply", test_command)
+    # you can directly register commands and their handlers
     chat.register_command("crayon", crayon_command)
+    chat.register_command("usage", usage_command)
 
     # we are done with our setup, lets start this bot up!
     chat.start()
---
+++ twitch/src/twitch_crayon/crayon.py
@@ -0,0 +1,129 @@
+from .colors import COLORS, EFFECTS
+
+import httpx
+import os
+import re
+import unicodedata
+
+type Color = tuple[int, int, int]
+type Effect = str | None
+
+
+class CrayonTwitchController:
+
+    _re_anycolor = re.compile(
+        r"""
+        ^\s*(
+            # Match RGB format: r,g,b
+            (
+                (?P<rd>\d+)\s*,
+                \s*(?P<gd>\d+)\s*,
+                \s*(?P<bd>\d+)
+            )
+            |
+            # Match Hex format: #RRGGBB
+            \#(
+                (?P<rh6>[0-9a-fA-F]{2})
+                (?P<gh6>[0-9a-fA-F]{2})
+                (?P<bh6>[0-9a-fA-F]{2})
+            )
+            |
+            # Match short Hex format: #RGB
+            \#(
+                (?P<rh3>[0-9a-fA-F])
+                (?P<gh3>[0-9a-fA-F])
+                (?P<bh3>[0-9a-fA-F])
+            )
+            |
+            # Match named color or effect
+            (?P<name>\w+)
+        )
+        """,
+        re.VERBOSE,
+    )
+
+    def __init__(self, base_url: str, token: str):
+        self.base_url = base_url
+        self.headers = {
+            "Authorization": f"Bearer {token}",
+            "content-type": "application/json",
+        }
+
+    def parse_color_param(self, param: str) -> tuple[Color, Effect]:
+        """
+        Parse a color parameter string and return the corresponding RGB color
+        and effect.
+        """
+        if not (m := self._re_anycolor.match(param)):
+            raise ValueError("Invalid color parameter format")
+        if m["rd"] and m["gd"] and m["bd"]:
+            r = int(m["rd"])
+            g = int(m["gd"])
+            b = int(m["bd"])
+            effect = None
+            if r > 255 or g > 255 or b > 255:
+                raise ValueError("Color values must be between 0 and 255")
+        elif m["rh6"] and m["gh6"] and m["bh6"]:
+            r = int(m["rh6"], 16)
+            g = int(m["gh6"], 16)
+            b = int(m["bh6"], 16)
+            effect = None
+        elif m["rh3"] and m["gh3"] and m["bh3"]:
+            r = int(m["rh3"] * 2, 16)
+            g = int(m["gh3"] * 2, 16)
+            b = int(m["bh3"] * 2, 16)
+            effect = None
+        elif m["name"]:
+            name = (
+                unicodedata.normalize("NFD", m["name"])
+                .encode("ascii", "ignore")
+                .decode("utf-8")
+                .capitalize()
+            )
+            if name in COLORS:
+                r, g, b = COLORS[name]
+                effect = None
+            elif name in EFFECTS:
+                r, g, b = 0, 0, 0
+                effect = name
+            else:
+                raise ValueError(f"Unknown color or effect name: {name}")
+        else:
+            raise ValueError("Invalid color parameter format")
+        return (r, g, b), effect
+
+    async def execute_light_effect(self, color: Color, effect: Effect) -> bool:
+        r, g, b = color
+        data = {
+            "entity_id": "light.crayon_rgb_light",
+            "effect": effect or "None",
+            "brightness": 255,
+        }
+        if effect is None:
+            data["rgb_color"] = [r, g, b]
+
+        async with httpx.AsyncClient() as client:
+            response = await client.post(
+                self.base_url + "api/services/light/turn_on",
+                headers=self.headers,
+                json=data,
+            )
+            return response.status_code == httpx.codes.OK
+
+    async def do_twitch_command(self, parameter: str):
+        if len(parameter) == 0:
+            return "J'ai besoin d'un paramètre. -> !usage"
+
+        try:
+            color, effect = self.parse_color_param(parameter)
+        except ValueError:
+            return "J'ai pas pu lire la couleur. -> !usage"
+
+        success = await self.execute_light_effect(color, effect)
+        if success:
+            return "Et voilà !"
+        else:
+            return (
+                "J'ai pas pu changer la couleur du crayon, mais c'est pas toi, "
+                "c'est ma faute."
+            )
